639ed102009c21815082e8268c45e094bbff17fc
Toxic-Cobra-Hub <psalinasangamarca@students.sjcny.edu>
2021-02-11 21:47:02 -0500

    My yearbook pablo

---
+++ app/src/main/java/com/example/groupyearbook/PablosActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupyearbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PablosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pablitos);
+    }
+}
