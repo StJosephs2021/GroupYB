9a1c9883db9bab6f33d311a59a3eadc03539ee83
StJosephs2021 <mmancini@student.sjcny.edu>
2021-02-17 15:08:51 -0500

    Added links to Next buttons.

--- app/src/main/java/com/example/groupyearbook/NickActivity.java
+++ app/src/main/java/com/example/groupyearbook/NickActivity.java
@@ -8,16 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class NickActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nickactivity);
     }
 
     public void buttonClick(View view){
-        Intent i = new Intent(getApplicationContext(),NickActivity.class);
+        Intent i = new Intent(getApplicationContext(),PablosActivity.class);
         startActivity(i);
     }
 }
