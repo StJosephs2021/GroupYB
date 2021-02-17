a544f34bd3145cd14ed0c960fcdd86684a928c47
StJosephs2021 <mmancini@student.sjcny.edu>
2021-02-17 15:08:51 -0500

    Added links to Next buttons.

--- app/src/main/java/com/example/groupyearbook/PablosActivity.java
+++ app/src/main/java/com/example/groupyearbook/PablosActivity.java
@@ -1,8 +1,12 @@
 package com.example.groupyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintSet;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class PablosActivity extends AppCompatActivity {
 
@@ -10,7 +14,10 @@ public class PablosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pablitos);
-
     }
 
+    public void buttonClick(View view){
+        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(i);
+    }
 }
