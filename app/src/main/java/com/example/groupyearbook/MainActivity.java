29e4128168d9b89618368885ecaf01d92a0eda77
StJosephs2021 <mmancini@student.sjcny.edu>
2021-02-25 21:10:29 -0500

    Added webview to my section of the app.

--- app/src/main/java/com/example/groupyearbook/MainActivity.java
+++ app/src/main/java/com/example/groupyearbook/MainActivity.java
@@ -5,19 +5,27 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.website);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
     }
-
-    public void buttonClick(View view){
-        Intent i = new Intent(getApplicationContext(),NickActivity.class);
-        startActivity(i);
+    public void switchNickActivity(View view){
+        Intent intent = new Intent(this, NickActivity.class);
+        startActivity(intent);
+    }
+    public void switchJohnActivity(View view){
+        Intent intent = new Intent(this, PablosActivity.class);
+        startActivity(intent);
     }
 }
