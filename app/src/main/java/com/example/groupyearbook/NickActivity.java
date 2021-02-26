52d4019ef580e7596903344403cea35383e38c28
Nick8889 <78318606+Nick8889@users.noreply.github.com>
2021-02-26 17:54:35 -0500

    Group Work Pt. 3

--- app/src/main/java/com/example/groupyearbook/NickActivity.java
+++ app/src/main/java/com/example/groupyearbook/NickActivity.java
@@ -29,7 +29,7 @@ public class NickActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nickactivity);
     }
 
-    public void buttonClick(View view) {
+    public void buttonClick (View view) {
         Intent i = new Intent(getApplicationContext(), PablosActivity.class);
         startActivity(i);
     }
